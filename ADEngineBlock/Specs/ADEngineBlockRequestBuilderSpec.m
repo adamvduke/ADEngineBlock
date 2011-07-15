8cea911204b12cdc555558dd6202c926e6c73ce6
Adam Duke <adam.v.duke@gmail.com>
2011-07-14 23:45:15 -0400

    made formatting changes in spec classes

--- ADEngineBlock/Specs/ADEngineBlockRequestBuilderSpec.m
+++ ADEngineBlock/Specs/ADEngineBlockRequestBuilderSpec.m
@@ -25,7 +25,7 @@ describe(@"ADEngineBlockRequestBuilder", ^{
             }
             fail(@"ADEngineBlockRequestBuilder failed to raise an exception");
         });
-        
+
         it(@"should raise an exception, when trying to init with blank key", ^{
             @try{
                 [[[ADEngineBlockRequestBuilder alloc] initWithAuthData:@"data" consumerKey:@"" consumerSecret:@"aSecret"] autorelease];
@@ -36,7 +36,7 @@ describe(@"ADEngineBlockRequestBuilder", ^{
             }
             fail(@"ADEngineBlockRequestBuilder failed to raise an exception");
         });
-        
+
         it(@"should raise an exception, when trying to init with blank secret", ^{
             @try{
                 [[[ADEngineBlockRequestBuilder alloc] initWithAuthData:@"data" consumerKey:@"aKey" consumerSecret:@""] autorelease];
@@ -50,14 +50,14 @@ describe(@"ADEngineBlockRequestBuilder", ^{
     });
 
     describe(@"valid requests", ^{
-        
+
         beforeEach(^{
             ADEngineBlockRequestBuilder *builder = [[[ADEngineBlockRequestBuilder alloc] initWithAuthData:@"data" 
                                                                                               consumerKey:@"key" 
                                                                                            consumerSecret:@"secret"]autorelease];
             [[SpecHelper specHelper].sharedExampleContext setObject:builder forKey:@"builder"];
         });
-        
+
         it(@"should produce a valid request for fetching the public timeline", ^{
             ADEngineBlockRequestBuilder *builder = [[SpecHelper specHelper].sharedExampleContext objectForKey:@"builder"];
             NSString *path = @"statuses/public_timeline.json";
@@ -69,7 +69,7 @@ describe(@"ADEngineBlockRequestBuilder", ^{
 //            }
 //            NSLog(@"%@", [request description]);
         });
-        
+
         it(@"should produce a valid request for updating a status", ^{
             ADEngineBlockRequestBuilder *builder = [[SpecHelper specHelper].sharedExampleContext objectForKey:@"builder"];
             NSString *path = @"statuses/update.json";
--- ADEngineBlock/Specs/NSString+CaseInsensitiveCompareSpec.m
+++ ADEngineBlock/Specs/NSString+CaseInsensitiveCompareSpec.m
@@ -12,11 +12,11 @@
 
 SPEC_BEGIN(CaseInsensitiveCompareSpec)
 describe(@"NSString+CaseInsensitiveCompare", ^{
-	
+
 	__block NSString *stringUUID = nil;
 	__block NSString *capitalCase = nil;
 	__block NSString *lowerCase = nil;
-	
+
 	beforeEach(^{
 		CFUUIDRef theUUID = CFUUIDCreate(NULL);
 		CFStringRef stringRef = CFUUIDCreateString(NULL, theUUID);
@@ -25,18 +25,18 @@ describe(@"NSString+CaseInsensitiveCompare", ^{
 		capitalCase = [stringUUID uppercaseString];
 		lowerCase = [stringUUID lowercaseString];
 	});
-	
+
 	afterEach(^{
 		[stringUUID release];
 	});
-	
+
 	it(@"should respond to isEqualIgnoreCase:", ^{
 		NSString *aString = @"aString";
 		NSAssert([aString respondsToSelector:@selector(isEqualIgnoreCase:)], @"NSString does not respond to isEqualIgnoreCase");
 	});
-	
+
 	it(@"should return true for a comparison of strings of lower case", ^{
-		
+
 		NSString *failureMessage = [NSString stringWithFormat:@"%@ is not equivalent to %@", capitalCase, lowerCase];
 		NSString *anotherLowerCase = [NSString stringWithString:lowerCase];
 		NSAssert(lowerCase != anotherLowerCase, @"The two strings point to the same object");
@@ -44,26 +44,26 @@ describe(@"NSString+CaseInsensitiveCompare", ^{
 	});
 
 	it(@"should return true for a comparison of strings of upper case", ^{
-		
+
 		NSString *failureMessage = [NSString stringWithFormat:@"%@ is not equivalent to %@, ignoring case", capitalCase, lowerCase];
 		NSString *anotherUpperCase = [NSString stringWithString:capitalCase];
 		NSAssert(capitalCase != anotherUpperCase, @"The two strings point to the same object");
 		NSAssert([capitalCase isEqualIgnoreCase:anotherUpperCase], failureMessage);
 	});
-	
+
 	it(@"should return true for a comparison of strings of different case", ^{
-		
+
 		NSString *failureMessage = [NSString stringWithFormat:@"%@ is not equivalent to %@, ignoring case", capitalCase, lowerCase];
 		NSAssert([capitalCase isEqualIgnoreCase:lowerCase], failureMessage);
 	});
-	
+
 	it(@"should return false for a comparison of different strings", ^{
-		
+
 		NSString *notAUUID = @"OMGnowaI";
 		NSString *failureMessage = [NSString stringWithFormat:@"%@ is equivalent to %@, ignoring case", capitalCase, notAUUID];
 		BOOL equivalent = [capitalCase isEqualIgnoreCase:notAUUID];
 		NSAssert(equivalent == NO , failureMessage);
 	});
-	
+
 });
 SPEC_END
